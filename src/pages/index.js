df6c191784c3af51fad33ee594a70fae2290bd0b
Paul Michaels <pcmichaels@live.com>
2023-07-27 07:21:00 +0100

    Fix location bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import { Seo } from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = ({location}) => (
+  <Layout location={location}>
     <section className="py-5 text-center container">
       <div className="row py-lg-5">
         <div className="col-lg-6 col-md-8 mx-auto">
