fe45fab5269e7d9f20dcdeb2550f13f883e3abe5
Paul Michaels <pcmichaels@live.com>
2023-07-30 07:43:58 +0100

    Now compiling with the new page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
@@ -10,7 +10,27 @@ const IndexPage = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>      
-      <div></div>
+      <section className="py-5 text-center container">
+        <div className="row py-lg-5">
+          <div className="col-lg-6 col-md-8 mx-auto">
+            <h1 className="fw-light">Funny Yellow Life</h1>
+            <p className="lead text-muted">The Home of Little Comics and Jokes</p>
+            <StaticImage
+              src="../images/FunnyYellowLife.png"
+              width={300}
+              quality={95}
+              formats={["AUTO", "WEBP"]}
+              alt="A Gatsby astronaut"
+              className="img-fluid"
+            />
+
+          </div>
+        </div>
+        <div className="row">
+          <Link to="/about/" className="btn btn-primary my-2">About</Link>
+          <Link to="/balloon/" className="btn btn-secondary my-2">Balloon</Link>
+        </div>
+      </section>
     </Layout>
   )
 }
