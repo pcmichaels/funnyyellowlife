7c77a48de85bcfe347af5a69837c4d9e53e0eb34
Paul Michaels <pcmichaels@live.com>
2023-07-30 07:38:58 +0100

    Remove blogs

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,34 +5,26 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import { Seo } from "../components/seo"
 
-const IndexPage = ({location}) => (
-  <Layout location={location}>
-    <section className="py-5 text-center container">
-      <div className="row py-lg-5">
-        <div className="col-lg-6 col-md-8 mx-auto">
-          <h1 className="fw-light">Funny Yellow Life</h1>
-          <p className="lead text-muted">The Home of Little Comics and Jokes</p>
-          <StaticImage
-            src="../images/FunnyYellowLife.png"
-            width={300}
-            quality={95}
-            formats={["AUTO", "WEBP"]}
-            alt="A Gatsby astronaut"
-            className="img-fluid"
-          />
+const IndexPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
 
-        </div>
-      </div>
-      <div className="row">
-        <Link to="/about/" className="btn btn-primary my-2">About</Link>
-        <Link to="/balloon/" className="btn btn-secondary my-2">Balloon</Link>
-      </div>
-    </section>
-  </Layout>
-)
+  return (
+    <Layout location={location} title={siteTitle}>      
+      <div></div>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
-export const Head = () => (
-    <Seo />
-)
+export const Head = () => <Seo title="All posts" />
+
+export const pageQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
